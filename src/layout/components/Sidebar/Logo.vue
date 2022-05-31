2253b858bff01a855c4da4efd5a74ac25c81ff83
D <2437757398@qq.com>
2022-05-31 18:52:47 +0800

    人事后台管理系统

---
+++ src/layout/components/Sidebar/Logo.vue
@@ -0,0 +1,84 @@
+<!--
+ * @Author: Utopia
+ * @Descripttion: Utopia 的代码
+ * @Date: 2022-05-31 16:47:30
+ * @LastEditTime: 2022-05-31 17:59:06
+ * @Description: file content
+-->
+<template>
+  <div class="sidebar-logo-container" :class="{'collapse':collapse}">
+    <transition name="sidebarLogoFade">
+      <router-link key="collapse" class="sidebar-logo-link" to="/">
+        <img src="@/assets/common/head.jpg" class="sidebar-logo">
+      </router-link>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SidebarLogo',
+  props: {
+    collapse: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data() {
+    return {
+      title: 'Vue Admin Template',
+      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.sidebarLogoFade-enter-active {
+  transition: opacity 1.5s;
+}
+
+.sidebarLogoFade-enter,
+.sidebarLogoFade-leave-to {
+  opacity: 0;
+}
+
+.sidebar-logo-container {
+  position: relative;
+  width: 100%;
+  // height: 200px;
+  // background: #2b2f3a;
+  text-align: center;
+  overflow: hidden;
+
+  & .sidebar-logo-link {
+    height: 100%;
+    width: 100%;
+
+    & .sidebar-logo {
+      width: 100%;
+      vertical-align: middle;
+      margin-right: 12px;
+    }
+
+    & .sidebar-title {
+      display: inline-block;
+      margin: 0;
+      color: #fff;
+      font-weight: 600;
+      line-height: 50px;
+      font-size: 14px;
+      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
+      vertical-align: middle;
+    }
+  }
+
+  &.collapse {
+    .sidebar-logo {
+      margin-right: 0px;
+      width: 32px;
+      height: 32px;
+    }
+  }
+}
+</style>
